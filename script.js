fa0c08b5e0df5df56ed1510328ac8f7a1ed8fc06
Maik Engel <maik.engel7@gmail.com>
2023-02-22 10:58:28 +0100

    Function Done

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let pokemonData = [];
 
+let maxPokemon = 12;
+let currentPokemon = 0;
+let nextPokemon = 12;
+
 const onInit = () => {
   fetchPokemon();
-  setTimeout(() => { 
-    renderPokemon();
-  }, 2000);
 };
 
 const fetchPokemon = () => {
@@ -15,24 +16,75 @@ const fetchPokemon = () => {
         pokemonData.push({
           id: data.id,
           name: data.name,
-          sprite: data.sprites.other,
+          sprite: data.sprites.other["official-artwork"].front_default,
+          types: data.types,
         });
+        if (pokemonData.length === 890) {
+          renderPokemon();
+        }
       })
       .catch((error) => console.error(error));
-    console.log(pokemonData);
   }
 };
 
 const renderPokemon = () => {
-  for (let i = 0; i < pokemonData.length; i++) {
-    let pokemonDataSort = pokemonData.sort((a, b) => a.id - b.id);
-    const pokemonImageContainer = document.querySelector(
-      "#pokemonImageContainer"
-    );
-    console.log(pokemonDataSort);
-    const pokemonImage = document.createElement("img");
+  let pokemonDataSort = pokemonData.sort((a, b) => a.id - b.id);
+  let loadMoreButton = document.getElementById("loadMoreButton");
+  for (let i = currentPokemon; i < maxPokemon; i++) {
+    let pokemonList = document.querySelector("#pokemonList");
+    let pokemonContainer = document.createElement("div");
+    pokemonContainer.setAttribute("class", "info-container");
+    pokemonList.appendChild(pokemonContainer);
+    rednerImageContainer(pokemonContainer, pokemonDataSort, i);
+    renderIDContainer(pokemonContainer, pokemonDataSort, i);
+    renderNameContainer(pokemonContainer, pokemonDataSort, i);
+    renderTypesContainer(pokemonContainer, pokemonDataSort, i);
+  }
+  loadMoreButton.classList.remove('d-none')
+  maxPokemon += nextPokemon;
+  currentPokemon += nextPokemon;
+};
 
-    pokemonImage.src = pokemonDataSort[i].sprite.home.front_default;
-    pokemonImageContainer.appendChild(pokemonImage);
+const rednerImageContainer = (pokemonContainer, pokemonDataSort, i) => {
+  let pokemonImageContainer = document.createElement("img");
+  let pokemonImage = pokemonDataSort[i].sprite;
+  pokemonImageContainer.classList.add("img-layout");
+  pokemonImageContainer.src = pokemonImage;
+  pokemonContainer.appendChild(pokemonImageContainer);
+};
+
+const renderIDContainer = (pokemonContainer, pokemonDataSort, i) => {
+  let pokemonIDContainer = document.createElement("h5");
+  let pokemonID = pokemonDataSort[i].id;
+  pokemonIDContainer.setAttribute("id", pokemonID);
+  pokemonIDContainer.innerHTML =
+    "#" + (pokemonID * 0.001).toFixed(3).toString().replace(".", "");
+  pokemonContainer.appendChild(pokemonIDContainer);
+};
+
+const renderNameContainer = (pokemonContainer, pokemonDataSort, i) => {
+  let pokemonNameContainer = document.createElement("h4");
+  let pokemonName = pokemonDataSort[i].name;
+  pokemonNameContainer.setAttribute("id", i);
+  pokemonNameContainer.innerHTML =
+    pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1);
+  pokemonContainer.appendChild(pokemonNameContainer);
+};
+
+const renderTypesContainer = (pokemonContainer, pokemonDataSort, i) => {
+  let pokemonTypesContainer = document.createElement("div");
+  pokemonTypesContainer.classList.add("d-flex", "gap-8");
+
+  for (let j = 0; j < pokemonDataSort[i].types.length; j++) {
+    let pokemonTypeContainer = document.createElement("p");
+    let pokemonType = pokemonDataSort[i].types[j].type.name;
+    pokemonTypeContainer.classList.add(
+      "background-color-" + pokemonType,
+      "pill"
+    );
+    pokemonTypeContainer.innerHTML =
+      pokemonType.charAt(0).toUpperCase() + pokemonType.slice(1);
+    pokemonTypesContainer.appendChild(pokemonTypeContainer);
+    pokemonContainer.appendChild(pokemonTypesContainer);
   }
 };

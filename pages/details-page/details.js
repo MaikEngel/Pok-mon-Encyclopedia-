45ac90b62fda15160912ff65f147240441e598e4
Maik Engel <maik.engel7@gmail.com>
2023-02-22 16:05:29 +0100

    start details-page

--- pages/details-page/details.js
+++ pages/details-page/details.js
@@ -1,14 +1,50 @@
+pokemonInfo = [];
+pokemonSpecies = [];
+
 const urlParams = new URLSearchParams(window.location.search);
-const pokemonName = urlParams.get('name');
+const pokemonName = urlParams.get("name");
+const pokemonDetailsContainer = document.getElementById("pokemonDetails");
 
-const pokemonDetailsContainer = document.getElementById('pokemonDetails');
+let detailsName;
+let detailsID;
 
-fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemonName}/`)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
+const fetchImages = () => {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}/`)
+    .then((response) => response.json())
+    .then((data) => {
+      pokemonInfo.push(data);
+    })
+    .catch((error) => console.error(error));
+  console.log(pokemonInfo);
+  console.log(pokemonSpecies);
+  fetchPokemonDetails();
+};
 
-        pokemonDetailsContainer.innerHTML = `
-      <h2>${data.name}</h2>
-    `;
+const fetchPokemonDetails = () => {
+  fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemonName}/`)
+    .then((response) => response.json())
+    .then((data) => {
+      pokemonSpecies.push(data);
     });
+  renderDetailsPage();
+};
+
+const renderDetailsPage = () => {
+  renderHeaderContainer();
+};
+
+const renderHeaderContainer = () => {
+  setTimeout(() => {
+    renderIdAndName();
+  }, 1000);
+};
+
+renderIdAndName = () => {
+  let headerContainer = document.getElementById("detailsHeaderContainerName");
+  detailsID = pokemonInfo[0].id;
+  detailsName = pokemonInfo[0].name;
+  headerContainer.innerHTML = `
+  #${(detailsID * 0.001).toFixed(3).toString().replace(".", "")}
+  ${detailsName.charAt(0).toUpperCase() + detailsName.slice(1)}
+  `;
+};
